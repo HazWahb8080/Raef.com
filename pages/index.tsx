68e777dd38438b0680feff1a0738d8d0abff30f5
HazWahb8080 <hazem.wahb100@gmail.com>
2023-02-10 17:49:48 +0200

    typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Reaf Alaa - your graphic designer</title>
+        <title>Reaef Alaa - your graphic designer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-[url('/pattern.svg')] h-full w-full">

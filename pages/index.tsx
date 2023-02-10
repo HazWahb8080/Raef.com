ec23020e6afa3558e5f6c3b5f84c9e1f4ca90a0b
HazWahb8080 <hazem.wahb100@gmail.com>
2023-02-10 14:33:59 +0200

    initial

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Reaf Alaa your graphic designer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className=""></main>
+    </div>
+  );
+};
+
+export default Home;
